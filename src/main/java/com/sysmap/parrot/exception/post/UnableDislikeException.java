4e98848a978fb05d7bc0d96f5b5190ba9db781d4
will763 <killzasper@gmail.com>
2023-05-09 19:25:55 -0300

    feat: added post exceptions

---
+++ src/main/java/com/sysmap/parrot/exception/post/UnableDislikeException.java
@@ -0,0 +1,11 @@
+package com.sysmap.parrot.exception.post;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+public class UnableDislikeException extends RuntimeException {
+    public UnableDislikeException(){
+        super();
+    }
+}
