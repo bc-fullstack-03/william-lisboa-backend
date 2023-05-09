9d59eeb1913b40a742aaddcae2056d1ac0043d36
will763 <killzasper@gmail.com>
2023-05-09 18:48:04 -0300

    commit initial

---
+++ src/main/java/com/sysmap/parrot/ParrotApplication.java
@@ -0,0 +1,13 @@
+package com.sysmap.parrot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParrotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParrotApplication.class, args);
+	}
+
+}
